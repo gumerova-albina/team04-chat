ed0b649ce2955df7ab6afae166b4d9056d9b96df
keleborn <Pro100keleborn>
2020-09-17 17:02:15 +0300

    Exit client corrected

--- server/src/main/java/com/chat/edu/Connection.java
+++ server/src/main/java/com/chat/edu/Connection.java
@@ -35,12 +35,13 @@ public class Connection implements Runnable{
                     }
                     // com.chat.edu.Server.clientSocketList...
                     // send com.chat.edu.Message to all other clients
-                }
-
-                if (clientLine.startsWith("/hist")) {
+                } else if (clientLine.startsWith("/hist")) {
+                } else if (clientLine.startsWith("/hist")) {
                     //History clientMessage = new History(clientLine);
 
                     // send History to exact client
+                } else if (clientLine.startsWith("/exit")){
+
                 }
             }
 
