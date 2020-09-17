7a23c7db532e2ed978cdba795ff03820a075f90f
ArseniyIFMO <vatrushka.96@mail.ru>
2020-09-17 11:44:19 +0300

    server Attempt

---
+++ src/main/java/Connection.java
@@ -0,0 +1,5 @@
+public class Connection extends Thread{
+    public Connection(){
+
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+    public static void main(String[] args) {
+        while(true) {
+            try (final ServerSocket connectionPortListener = new ServerSocket(10_000);
+                 final Socket clientConnection = connectionPortListener.accept();
+                 final DataInputStream input = new DataInputStream(
+                         new BufferedInputStream(
+                                 clientConnection.getInputStream()));
+                 final DataOutputStream out = new DataOutputStream(
+                         new BufferedOutputStream(
+                                 clientConnection.getOutputStream()))) {
+
+                Connection connection = new Connection();
+
+                final String readLine = input.readUTF();
+                out.writeUTF("!!! " + readLine); //INT 5
+                out.flush();
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
