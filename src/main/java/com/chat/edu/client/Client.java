7a52017006e386d7f543a40357876a198d651f1d
gumerova-albina <gumerova.albina38@gmail.com>
2020-09-18 12:15:23 +0300

    Fix typo

--- src/main/java/com/chat/edu/client/Client.java
+++ src/main/java/com/chat/edu/client/Client.java
@@ -3,7 +3,7 @@ package com.chat.edu.client;
 public class Client {
     public static void main(String[] args){
         if (args.length != 2) {
-            System.out.println("Please specify host and port and try again\");");
+            System.out.println("Please specify host and port and try again");
         }
         else {
             ClientController.start(args[0], Integer.valueOf(args[1]));
