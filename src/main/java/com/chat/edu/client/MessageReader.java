f03433634db48a0e62906164fe6d6efe10c27bd2
gumerova-albina <gumerova.albina38@gmail.com>
2020-09-18 09:51:03 +0300

    Add methods neccessary for tests

--- src/main/java/com/chat/edu/client/MessageReader.java
+++ src/main/java/com/chat/edu/client/MessageReader.java
@@ -8,19 +8,16 @@ import java.net.Socket;
 
 /**
  * Class that represents input messages
- *
  */
 
-public class MessageReader extends Thread{
+public class MessageReader extends Thread {
     private DataInputStream input;
     private Socket connection;
 
     public MessageReader(Socket connection) {
         this.connection = connection;
         try {
-            input = new DataInputStream(
-                    new BufferedInputStream(
-                            connection.getInputStream()));
+            input = createDataInputStream();
         } catch (IOException e) {
             System.out.println("Can't connect to get messages");
             e.printStackTrace();
@@ -28,8 +25,8 @@ public class MessageReader extends Thread{
     }
 
     @Override
-    public void run(){
-        while(!connection.isClosed()){
+    public void run() {
+        while (!connection.isClosed()) {
             try {
                 System.out.println(input.readUTF());
             } catch (EOFException e) {
@@ -39,5 +36,11 @@ public class MessageReader extends Thread{
                 e.printStackTrace();
             }
         }
+    }
+
+    DataInputStream createDataInputStream() throws IOException {
+        return new DataInputStream(
+                new BufferedInputStream(
+                        connection.getInputStream()));
     }
 }
