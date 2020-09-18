bef857a5134f7cabcf807f450c5dcd7a04348779
gumerova-albina <gumerova.albina38@gmail.com>
2020-09-18 12:12:42 +0300

    Fix try-catch in server

--- src/main/java/com/chat/edu/server/Server.java
+++ src/main/java/com/chat/edu/server/Server.java
@@ -11,12 +11,12 @@ import java.util.concurrent.ConcurrentHashMap;
 public class Server {
     public static Map<String, DataOutputStream> loginMap = new ConcurrentHashMap<String, DataOutputStream>();
 
-    public static List<Pair <DataInputStream, DataOutputStream>> collection = new ArrayList<>();
+    public static List<Pair<DataInputStream, DataOutputStream>> collection = new ArrayList<>();
 
     public static List<Message> messageList = new ArrayList<>();
 
     public static void main(String[] args) {
-       if (args.length != 1) {
+        if (args.length != 1) {
             System.out.println("Please specify port and try again");
         } else {
             final int port = Integer.valueOf(args[0]);
@@ -25,6 +25,9 @@ public class Server {
                     final Socket clientConnection = connectionPortListener.accept();
                     System.out.println("User joined chat");
                     new Thread(new MessageHandler(clientConnection)).start();
+                }
+            } catch (IOException e) {
+                System.out.println("Can't connect to client");
             }
         }
     }
